279e86a0e5db313380ce15b49516def726aeddfc
Tadej Birtic <tadejbirtic@gmail.com>
2017-11-10 16:46:05 +0100

    still not working commit

--- js/flip.js
+++ js/flip.js
@@ -2,7 +2,6 @@
  * Created by tadej on 11/9/2017.
  */
 const cards = document.querySelectorAll('.card');
-const body = document.querySelector('body');
 const back = document.querySelector('.back');
 const front = document.querySelector('.front');
 
@@ -14,10 +13,17 @@ const manipulateCard = (card) => {
                 card.setAttribute('opened', '1');
                 card.style.transform = 'rotate3d(0, 1, 0, 180deg)';
                 card.style.transition = 'transform 1s ease-in-out';
+                front.style.visibility = 'hidden';
+                back.style.visibility = 'visible';
+                back.style.transition = 'visibility 2s';
             } else {
                 card.setAttribute('opened', '0');
                 card.style.transform = 'rotate3d(0, 1, 0, 0deg)';
                 card.style.transition = 'transform 1s ease-in-out';
+                front.style.visibility = 'visible';
+                back.style.visibility = 'hidden';
+                front.style.transition = 'visibility 2s ease-in-out';
+                back.style.transition = 'visibility 2s ease-in-out';
             }
         })
 };
